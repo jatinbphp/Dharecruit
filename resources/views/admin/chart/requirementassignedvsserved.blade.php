2ae04351331c11bc929cc3c88d11c33f5b0a3bfb
jatinbphp <jatin.p.php@gmail.com>
2024-02-23 17:01:09 +0530

    fix issue.

--- resources/views/admin/chart/requirementassignedvsserved.blade.php
+++ resources/views/admin/chart/requirementassignedvsserved.blade.php
@@ -1,6 +1,6 @@
 <div class="card card-info">
     <div class="card-header">
-        <h3 class="card-title">Requirement Assigned Vs Served</h3>
+        <h3 class="card-title">Requirement Assigned And Served</h3>
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                 <i class="fas fa-minus"></i>
